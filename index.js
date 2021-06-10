7a2574fb60c1691b11cfa265998aac2bca886c24
Lakendria <kendra939@gmail.com>
2021-06-09 21:15:39 -0400

    react app created

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const express = require("express")
 const app = express()
 const cors = require ("cors")
 const port = process.env.PORT || 9000
+const path = require("path")
 //SET_UP ENDPOINTS
 //CREATED AN API
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
+
 app.use("/api/",(_,res)=>{
     res.json({data:"The API is serving data!!!!"})
 })
 
 app.listen(port, ()=>{
     console.log(`Server is alive on port ${port}`)
+})
+app.use("*", (_,res)=>{
+    res.sendFile(path.join(__dirname,"client/build","index.html"))
 })
 
 
