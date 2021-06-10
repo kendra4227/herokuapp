37072c0b98c47d8c60a0cdd21477fa04b4e1ca7a
Lakendria <kendra939@gmail.com>
2021-06-09 21:23:28 -0400

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-
 import './App.css';
 import {useState, useEffect} from 'react'
 
 function url(path){
-  return  process.env.NODE_ENV = "development" ? `http://localhost:1234${path}` :path
+  return  process.env.NODE_ENV === "development" ? `http://localhost:1234${path}` : path
 }
 
 function App() {
